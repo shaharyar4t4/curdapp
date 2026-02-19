c68e44847754d8998351596059bcb30005e4b658
shaharyar <shaharyar4t4@gmail.com>
2026-02-19 12:03:49 +0500

    lean about the some new packages (like apllo server, class validator and class transfromation ) make a book model and also make the dto of book input for validation

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,29 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
+import { BookModule } from './book/book.module';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverAsyncConfig, ApolloDriverConfig } from '@nestjs/apollo';
+import { ApolloBaseDriver } from '@nestjs/apollo/dist/drivers/apollo-base.driver';
+import { join } from 'path';
+
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true
     }),
+    // the graphql is inital add the on app.module.ts
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      // the below line is help auto create the file on schema.gql no need to create manually
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      sortSchema:true,
+      // the below is playground: true is provide the plateform where you test the queries... no need the POSTMAN
+      playground:true
+    }),
     MongooseModule.forRoot(process.env.MONGO_URI!),
+    BookModule,
   ],
   controllers: [AppController],
   providers: [AppService],
---
+++ src/book/model/book.model.ts
@@ -0,0 +1,28 @@
+import {Prop, Schema, SchemaFactory} from "@nestjs/mongoose"
+import {Document} from "mongoose";
+import { ObjectType, Field, ID } from "@nestjs/graphql";
+import { first } from "rxjs";
+
+@Schema()
+// this is comes from the object but tread as a object
+@ObjectType()
+export class Book extends Document{
+  
+    @Field(() => ID)
+    // the declare word is show the ID is only for Readonly
+    declare readonly_id: string;
+
+    @Prop({required: true})
+    @Field()
+    title: string;
+
+    @Prop()
+    @Field({nullable: true})
+    description?: string;
+
+    @Prop({required: true})
+    @Field()
+    author: string;
+}
+
+export const BookSchema = SchemaFactory.createForClass(Book);
