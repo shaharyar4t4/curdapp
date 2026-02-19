b7f0689ed74735a883ce96bb2cdba08480c166cd
shaharyar <shaharyar4t4@gmail.com>
2026-02-19 12:32:25 +0500

    complete the process of update book detial and book services

--- src/book/book.service.ts
+++ src/book/book.service.ts
@@ -1,4 +1,41 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Book } from './model/book.model';
+import { Model } from 'mongoose';
+import { CreateBookInput } from './dto/create.book.input';
+import { updateBookInput } from './dto/update.book.input';
 
 @Injectable()
-export class BookService {}
+export class BookService {
+
+    constructor(@InjectModel(Book.name) private bookModel: Model<Book>) { }
+
+    async createBookDetial(input: CreateBookInput): Promise<Book> {
+        const created = new this.bookModel(input);
+        return created.save();
+    }
+
+    async findAll(): Promise<Book[]> {
+        return this.bookModel.find().exec();
+    }
+
+    async findOne(id: string): Promise<Book> {
+        const book = await this.bookModel.findById(id).exec();
+        if (!book) throw new NotFoundException('Book Not Found')
+        return book;
+    }
+
+    async updateBookDetial(input: updateBookInput): Promise<Book> {
+        const existingBook = await this.bookModel.findById(input.id);
+        if (!existingBook) throw new NotFoundException('Book Not Found')
+        Object.assign(existingBook, input);
+        return existingBook.save();
+
+    }
+
+    async removeBookDetial(id: string): Promise<boolean> {
+        const result = await this.bookModel.findByIdAndDelete(id);
+        if (!result) throw new NotFoundException('Book Not Found')
+        return true;
+    }
+}
---
+++ src/book/dto/update.book.input.ts
@@ -0,0 +1,16 @@
+import { CreateBookInput } from "./create.book.input"
+import { InputType, Field, PartialType, ID } from "@nestjs/graphql"
+
+import { IsNotEmpty } from "class-validator"
+
+@InputType()
+
+// they not repect the user input bs yehe hum na CreateBookInput ki file ko extend karva daya ha 
+export class updateBookInput extends PartialType(CreateBookInput){
+    // Is file ko create karna ki waja yehe ha like ap jese Book ki ID ko update karha is ye file validate karaya gi 
+    @Field(() => ID)
+    @IsNotEmpty()
+    id: string
+
+    // ye id ko use karta ho hum field ko change karsaata ha 
+}
